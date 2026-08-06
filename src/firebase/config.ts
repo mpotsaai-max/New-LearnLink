f586230663fc3c14f0dc124db87ba32a0d0397ef
HectorS <mpotsa.ai@gmail.com>
2026-08-06 22:40:30 +0200

    corrected for deployments

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfigData from '../../firebase-applet-config.json';
+import firebaseConfigData from '../firebase-applet-config.json';
 
 const app = !getApps().length ? initializeApp(firebaseConfigData) : getApp();
 
